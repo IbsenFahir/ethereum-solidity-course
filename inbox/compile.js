a87244c4cc7668a3a6065c86c46f087c5c475731
Ibsen Fahir <ibsenfahir@gmail.com>
2021-11-28 15:28:45 -0300

    Add initial code for inbox smart contract

---
+++ inbox/compile.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+
+
+const input = {
+  language: "Solidity",
+  sources: {
+    // Each Solidity source file to be compiled must be specified by defining either
+    // a URL to the file or the literal file content.
+    // See https://docs.soliditylang.org/en/v0.8.5/using-the-compiler.html#input-description
+    "Inbox.sol": {
+      content: source
+    }
+  },
+  settings: {
+    metadata: {
+      useLiteralContent: true
+    },
+    outputSelection: {
+      "*": {
+        "*": ["*"]
+      }
+    }
+  }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log(output.contracts["Inbox.sol"].Inbox)
+module.exports = output.contracts["Inbox.sol"].Inbox;
